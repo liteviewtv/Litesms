d545244e0a34a7aafea09fa660b3ad1b227183b3
liteviewtv <liteviewtv@gmail.com>
2026-09-06 17:26:44 +0100

    Add accounts page

---
+++ src/accounts-page.js
@@ -0,0 +1,8 @@
+export default function AccountsPage() {
+  return (
+    <div className="page-placeholder">
+      <h1>Accounts</h1>
+      <p>Account products will appear here.</p>
+    </div>
+  )
+}
