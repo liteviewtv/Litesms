a8bb718e2394c145b5078d790e05b80aac9fd430
liteviewtv <liteviewtv@gmail.com>
2026-09-24 23:36:23 +0100

    Add logos for Clickentregas Flash and JD services

--- src/service-ui.js
+++ src/service-ui.js
@@ -5,6 +5,7 @@ const SERVICE_LOGOS = {
   microsoft: ['microsoft', '5E5CFF'], outlook: ['microsoftoutlook', '0078D4'], yahoo: ['yahoo', '6001D2'], skype: ['skype', '00AFF0'],
   viber: ['viber', '7360F2'], line: ['line', '00C300'], wechat: ['wechat', '07C160'], kakao: ['kakaotalk', 'FFCD00'], reddit: ['reddit', 'FF4500'],
   steam: ['steam', '000000'], uber: ['uber', '000000'], airbnb: ['airbnb', 'FF5A5F'], amazon: ['amazon', 'FF9900'], binance: ['binance', 'F0B90B'],
+  clickentregas: ['clickup', '7B68EE'], flash: ['flash', 'FFB300'], jd: ['jd', 'E1251B'],
   protonmail: ['protonmail', '6D4AFF'], proton: ['proton', '6D4AFF'], imo: ['imo', '00AEEF'], vk: ['vk', '0077FF'], twitch: ['twitch', '9146FF'],
   spotify: ['spotify', '1ED760'], youtube: ['youtube', 'FF0000'], github: ['github', '181717'], microsoftteams: ['microsoftteams', '6264A7'],
   teams: ['microsoftteams', '6264A7'], openai: ['openai', '000000'], chatgpt: ['openai', '000000']
@@ -21,7 +22,7 @@ const serviceKey = (text) => aliases[normalize(text)] || normalize(text);
 
 const POPULAR_SERVICES = new Set(['whatsapp','telegram','google','instagram','facebook','tiktok','discord','twitter','x']);
 const SERVICE_DOMAINS = {
-  aliexpress:'aliexpress.com', alibaba:'alibaba.com', apple:'apple.com', avito:'avito.ru', blablacar:'blablacar.com', booking:'booking.com', bolt:'bolt.eu', careem:'careem.com', cashapp:'cash.app', clubhouse:'clubhouse.com', coinbase:'coinbase.com', craigslist:'craigslist.org', deezer:'deezer.com', deliveroo:'deliveroo.com', doordash:'doordash.com', dropbox:'dropbox.com', ebay:'ebay.com', etsy:'etsy.com', eventbrite:'eventbrite.com', indeed:'indeed.com', kakaopay:'kakaopay.com', lazada:'lazada.com', lyft:'lyft.com', mercadolibre:'mercadolibre.com', netflix:'netflix.com', okcupid:'okcupid.com', pinterest:'pinterest.com', quora:'quora.com', roblox:'roblox.com', shopee:'shopee.com', slack:'slack.com', tinder:'tinder.com', uber:'uber.com', venmo:'venmo.com', walmart:'walmart.com', wise:'wise.com', yahoo:'yahoo.com', zalando:'zalando.com'
+  clickentregas:'clickentregas.com', flash:'flash.co', jd:'jd.com', aliexpress:'aliexpress.com', alibaba:'alibaba.com', apple:'apple.com', avito:'avito.ru', blablacar:'blablacar.com', booking:'booking.com', bolt:'bolt.eu', careem:'careem.com', cashapp:'cash.app', clubhouse:'clubhouse.com', coinbase:'coinbase.com', craigslist:'craigslist.org', deezer:'deezer.com', deliveroo:'deliveroo.com', doordash:'doordash.com', dropbox:'dropbox.com', ebay:'ebay.com', etsy:'etsy.com', eventbrite:'eventbrite.com', indeed:'indeed.com', kakaopay:'kakaopay.com', lazada:'lazada.com', lyft:'lyft.com', mercadolibre:'mercadolibre.com', netflix:'netflix.com', okcupid:'okcupid.com', pinterest:'pinterest.com', quora:'quora.com', roblox:'roblox.com', shopee:'shopee.com', slack:'slack.com', tinder:'tinder.com', uber:'uber.com', venmo:'venmo.com', walmart:'walmart.com', wise:'wise.com', yahoo:'yahoo.com', zalando:'zalando.com'
 };
 function addGenericServiceLogo(wrap, raw) {
   const slug = normalize(raw);
